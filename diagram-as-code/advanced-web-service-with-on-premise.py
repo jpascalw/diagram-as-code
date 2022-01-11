da7d3b672c532f3e8d2da4251098f6cdb3f0883a
Pascal Wallenius <pascal.wallenius@volvocars.com>
2022-01-11 18:12:39 +0100

    edit diagram main label

--- diagram-as-code/advanced-web-service-with-on-premise.py
+++ diagram-as-code/advanced-web-service-with-on-premise.py
@@ -8,7 +8,7 @@ from diagrams.onprem.monitoring import Grafana, Prometheus
 from diagrams.onprem.network import Nginx
 from diagrams.onprem.queue import Kafka
 
-with Diagram(name="Advanced Web Service with On-Premise (colored)", show=False):
+with Diagram(name="Advanced Web Service with On-Premise", show=False):
     ingress = Nginx("ingress")
 
     metrics = Prometheus("metric")
